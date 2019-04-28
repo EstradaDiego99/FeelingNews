64f8af476924f53af5945f9b8ba9c1debf8886ec
EstradaDiego99 <devdiegoest@gmail.com>
2019-04-28 11:53:44 -0500

    Semi fracaso

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'csv'
+require "json"
+require "ibm_watson/natural_language_understanding_v1"
+include IBMWatson
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +14,11 @@ module FeelingNews
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    $natural_language_understanding = IBMWatson::NaturalLanguageUnderstandingV1.new(
+      version: "2018-11-16",
+      iam_apikey: "sZ3rzDvR06-X4kHsBtNqb7GUpqEQJ23Jiuf_QYMgxKAL",
+      url: "https://gateway.watsonplatform.net/natural-language-understanding/api"
+    )
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
